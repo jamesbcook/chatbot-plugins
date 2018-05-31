b6aab7309af7efc702d64d4d6b82f05323344f12
James Cook <jbcook@protonmail.com>
2018-05-31 11:02:32 -0700

    Update to coin market cap v2 api
    Now that we use the v2 api, we have a loop that requests the list of
    coins and stores the name and symbol in a hashmap.
    
    When a request comes in we check the two hashmaps, and if it exists we
    send a request to the api looking for the current price.

--- cryptocurrency/main.go
+++ cryptocurrency/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/jamesbcook/chatbot/kbchat"
 )
 
 const (
-	userAgent = "KeyBase Chatbot"
-	urlFMT    = "https://api.coinmarketcap.com/v1/ticker/%s/?convert=USD"
+	userAgent  = "KeyBase Chatbot"
+	listingURL = "https://api.coinmarketcap.com/v2/listings/"
+	priceURL   = "https://api.coinmarketcap.com/v2/ticker/%d/?convert=BTC"
 )
 
 var (
@@ -22,6 +25,8 @@ var (
 	Help         = "/crypto {cryptocurrency}"
 	areDebugging = false
 	debugWriter  *io.Writer
+	nameMap      = make(map[string]int)
+	symbolMap    = make(map[string]int)
 )
 
 type getting string
@@ -35,14 +40,56 @@ var Sender getting
 //Debugger export Symbol
 var Debugger getting
 
-//CryptoOutput is the results from the api
-type CryptoOutput struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	Symbol   string `json:"symbol"`
-	Rank     string `json:"rank"`
-	PriceUSD string `json:"price_usd"`
-	PriceBTC string `json:"price_btc"`
+//Listing results
+type listing struct {
+	Data []struct {
+		ID          int    `json:"id"`
+		Name        string `json:"name"`
+		Symbol      string `json:"symbol"`
+		WebsiteSlug string `json:"website_slug"`
+	} `json:"data"`
+	MetaData struct {
+		TimeStamp           int64       `json:"timestamp"`
+		NumCryptoCurrencies int         `json:"num_cryptocurrencies"`
+		Error               interface{} `json:"error"`
+	} `json:"metadata"`
+}
+
+//TickerSpecific for a currency
+type tickerSpecific struct {
+	Data struct {
+		ID                int     `json:"id"`
+		Name              string  `json:"name"`
+		Symbol            string  `json:"symbol"`
+		WebsiteSlug       string  `json:"website_slug"`
+		Rank              int     `json:"rank"`
+		CirculatingSupply float64 `json:"circulating_supply"`
+		TotalSupply       float64 `json:"total_supply"`
+		MaxSupply         float64 `json:"max_supply"`
+		Quotes            struct {
+			USD struct {
+				Price            float64 `json:"price"`
+				Volume24H        float64 `json:"volume_24h"`
+				MarketCap        float64 `json:"market_cap"`
+				PercentChange1H  float64 `json:"percent_change_1h"`
+				PercentChange24H float64 `json:"percent_change_24h"`
+				PercentChange7D  float64 `json:"percent_change_7d"`
+			} `json:"USD"`
+			BTC struct {
+				Price            float64 `json:"price"`
+				Volume24H        float64 `json:"volume_24h"`
+				MarketCap        float64 `json:"market_cap"`
+				PercentChange1H  float64 `json:"percent_change_1h"`
+				PercentChange24H float64 `json:"percent_change_24h"`
+				PercentChange7D  float64 `json:"percent_change_7d"`
+			} `json:"BTC"`
+		} `json:"quotes"`
+		LastUpdated int64 `json:"last_updated"`
+	} `json:"data"`
+	Metadata struct {
+		Timestamp int64       `json:"timestamp"`
+		Error     interface{} `json:"error"`
+	} `json:"metadata"`
 }
 
 func (g getting) Debug(set bool, writer *io.Writer) {
@@ -57,36 +104,95 @@ func debug(input string) {
 	}
 }
 
+func updateListing() {
+	for {
+		l := &listing{}
+		req, err := http.NewRequest("GET", listingURL, nil)
+		if err != nil {
+			errMsg := fmt.Errorf("[Crypto Error] making http request %v", err)
+			debug(errMsg.Error())
+			continue
+		}
+		req.Header.Set("User-Agent", userAgent)
+		client := &http.Client{}
+		debug(fmt.Sprintf("Sending request %v", req))
+		resp, err := client.Do(req)
+		if err != nil {
+			errMsg := fmt.Errorf("[Crypto Error] making do request %v", err)
+			debug(errMsg.Error())
+			continue
+		}
+		defer resp.Body.Close()
+		buf, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			errMsg := fmt.Errorf("[Crypto Error] reading resp body %v", err)
+			debug(errMsg.Error())
+			continue
+		}
+		debug(fmt.Sprintf("response length %d", len(buf)))
+		if err := json.Unmarshal(buf, l); err != nil {
+			errMsg := fmt.Errorf("[Crypto Error] unmarshal request %v", err)
+			debug(errMsg.Error())
+			continue
+		}
+		for _, coin := range l.Data {
+			name := strings.ToLower(coin.Name)
+			symbol := strings.ToLower(coin.Symbol)
+			nameMap[name] = coin.ID
+			symbolMap[symbol] = coin.ID
+		}
+		//API updates every five mintues
+		time.Sleep(5 * time.Minute)
+	}
+}
+
 //Get export method that satisfies an interface in the main program.
 //This Get method will query the coinmarketcap api.
 func (g getting) Get(input string) (string, error) {
-	url := fmt.Sprintf(urlFMT, input)
-	client := &http.Client{}
-	debug(fmt.Sprintf("Creating GET request to %s", url))
+	debug(fmt.Sprintf("Got Input: %s", input))
+	var id int
+	lowerInput := strings.ToLower(input)
+	if _, ok := nameMap[lowerInput]; ok {
+		id = nameMap[lowerInput]
+	} else if _, ok := symbolMap[lowerInput]; ok {
+		id = symbolMap[lowerInput]
+	} else {
+		err := fmt.Errorf("%s not found", input)
+		debug(err.Error())
+		return "", err
+	}
+	url := fmt.Sprintf(priceURL, id)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", fmt.Errorf("[Crypto Error] creating request %v", err)
+		errMsg := fmt.Errorf("[Crypto Error] new request %v", err)
+		debug(errMsg.Error())
+		return "", errMsg
 	}
 	req.Header.Set("User-Agent", userAgent)
-	debug(fmt.Sprintf("Sending request %v", req))
+	client := &http.Client{}
+	debug(fmt.Sprintf("Sending req %v", req))
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", fmt.Errorf("[Crypto Error] sending request %v", err)
+		errMsg := fmt.Errorf("[Crypto Error] do request %v", err)
+		debug(errMsg.Error())
+		return "", errMsg
 	}
 	defer resp.Body.Close()
-	var buf bytes.Buffer
-	debug(fmt.Sprintf("Reading resp.Body"))
-	_, err = buf.ReadFrom(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", fmt.Errorf("[Crypto Error] reading from resp body %v", err)
+		errMsg := fmt.Errorf("[Crypto Error] reading resp body %v", err)
+		debug(errMsg.Error())
+		return "", errMsg
 	}
-	crypto := []CryptoOutput{}
-	debug(fmt.Sprintf("Unmarshalling json with length of %d", len(buf.Bytes())))
-	if err := json.Unmarshal(buf.Bytes(), &crypto); err != nil {
-		return "", fmt.Errorf("[Crypto Error] unmarshalling response %v", err)
+	debug(fmt.Sprintf("Response size %d", len(body)))
+	t := &tickerSpecific{}
+	if err := json.Unmarshal(body, t); err != nil {
+		errMsg := fmt.Errorf("[Crypto Error] unmarshal %v", err)
+		debug(errMsg.Error())
+		return "", errMsg
 	}
-	output := fmt.Sprintf("Name: %-8s\tUSD: %-6s\tBTC: %-10s", crypto[0].Name, crypto[0].PriceUSD, crypto[0].PriceBTC)
-	debug(fmt.Sprintf("Message sending to user\n%s", output))
+	output := fmt.Sprintf("Name: %-8s\tUSD: %-6.2f\tBTC: %-10f", t.Data.Name, t.Data.Quotes.USD.Price, t.Data.Quotes.BTC.Price)
+	debug(fmt.Sprintf("Returning %s", output))
 	return output, nil
 }
 
@@ -96,7 +202,9 @@ func (g getting) Send(msgID, msg string) error {
 	debug("Starting kbchat")
 	w, err := kbchat.Start("chat")
 	if err != nil {
-		return fmt.Errorf("[Crypto Error] sending message %v", err)
+		errMsg := fmt.Errorf("[Crypto Error] sending message %v", err)
+		debug(errMsg.Error())
+		return errMsg
 	}
 	debug(fmt.Sprintf("Sending this message to messageID: %s\n%s", msgID, msg))
 	if err := w.SendMessage(msgID, msg); err != nil {
